5f8edd461425a417510f9c87867b02ae18129894
PHMM Mihilanga <139644752+MIHILANGA@users.noreply.github.com>
2024-02-02 13:34:44 +0530

    new

---
+++ client/src/Home.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+function Signup(){
+
+    return(
+        <div>
+            <div>
+                <h2>Home Page</h2>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const EmployeeModel = require('./models/Employee')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://admin:Miyuru4302@miyuruapi.imhpf7h.mongodb.net/Leave_System?retryWrites=true&w=majority");
+
+
+app.post("/login",(req,res)=>{
+    const{email,password}=req.body;
+    EmployeeModel.findOne({email: email})
+    .then(user =>{
+        if(user){
+            if(user.password === password){
+                res.json("Success")
+            }else{
+                res.json("the password is incorrect")
+            }
+        }else {
+            res.json("No record existed")
+        }
+    })
+})
+
+app.post('/register',(req,res)=>{
+    EmployeeModel.create(req.body)
+    .then(employees => res.json(employees))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, ()=>{
+    console.log("server is running")
+})
