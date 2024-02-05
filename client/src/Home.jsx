5ed4bc0165357709997cca2655a0618fb160b173
PHMM Mihilanga <139644752+MIHILANGA@users.noreply.github.com>
2024-02-05 13:26:08 +0530

    2/5

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,22 +1,43 @@
-
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
-import './CSS/Signup.css'; // Import your CSS file
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CSS/Home.css'; // Import your CSS file
 
 function Signup() {
-    
-
     return (
         <div className="signup-container">
             <div className="header-rectangle">
-            <img className="logo" alt="Kotelawala defence" src="kdu.png" />
+                <img className="logo" alt="Kotelawala defence" src="kdu.png" />
                 <h1>Leave Management System</h1>
             </div>
-               
-                </div>
-            
+            <div className="nav-buttons">
+                <Link to="/FormD" className="requestsbtn">
+                    Requests
+                </Link>
+                <Link to="/VehicleDetails" className="vehiclesbtn">
+                    Vehicles
+                </Link>
+                <Link to="/Assign" className="assignbtn">
+                    Assign
+                </Link>
+                <Link to="/DriversDetails" className="driverbtn">
+                    Drivers
+                </Link>
+                <Link to="/SpecialRequest" className="reservationbtn">
+                    Reservations
+                </Link>
+            </div>
+            {/* Panel section */}
+            <div className="panel">
+                {/* Your panel content goes here */}
+                <h2>Student Requests (DaySchoolers , Cadets)</h2>
+                
+            </div>
+            <div className="panel2">
+                {/* Your panel content goes here */}
+                <h2>Other Requests (Officers , Staff)</h2>
+                
+            </div>
+        </div>
     );
 }
 
